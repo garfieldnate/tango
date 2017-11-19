a768d4064eb6e302b8fee16c29cfc2f1bd3eb6f4
Nathan Glenn <nathan-gary.glenn@trivago.com>
2017-11-19 14:51:16 +0100

    remove obsolete migrate command

--- tango/cli.py
+++ tango/cli.py
@@ -4,7 +4,6 @@ import click
 
 from .commands.add import tui as tui_add
 from .commands.study import tui as tui_study
-from .commands.migrate import migrate as db_migrate
 
 @click.group()
 def main():
@@ -20,10 +19,6 @@ def add(language, headword):
 @click.argument('language', default='all')
 def study(language):
     tui_study(language)
-
-@main.command()
-def migrate():
-    db_migrate()
 
 if __name__ == "__main__":
     main()
